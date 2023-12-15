e1e782a0245983eadc9e0662d282697e01ff98b8
honorine <honorineru@gmail.com>
2023-12-15 11:10:31 +0100

    index

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-});
-
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
 });
@@ -37,3 +33,11 @@ io.on('connection', (socket) => {
         io.emit('chat message', msg);
     });
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+  
